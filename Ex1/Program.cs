20649053a3b8cde122e71b3ff1029afe71375823
Alexandr <algran83@mail.ru>
2022-11-01 01:28:28 +0300

    add Ex's

---
+++ Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число для проверки на палиндром: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 9999 && num < 100000) 
+    {
+        int num5 = num % 10;
+        int num4 = num/10 % 10;
+        int num3 = num/100 % 10;
+        int num2 = num/1000 % 10;
+        int num1 = num/10000 % 10;
+        
+        if (num5 == num1 && num4 == num2) System.Console.WriteLine($"Число {num} -> палиндром");
+        else System.Console.WriteLine($"Число {num} ->  не палиндром");
+    }
+else System.Console.WriteLine("Нужно ввести пятизначное число");
---
+++ Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("Введите координаты X, Y. Z для точки A: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+int yA = Convert.ToInt32(Console.ReadLine());
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X, Y. Z для точки A: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+int yB = Convert.ToInt32(Console.ReadLine());
+int zB = Convert.ToInt32(Console.ReadLine());
+
+double length = Math.Sqrt(Math.Pow(xB-xA, 2) + Math.Pow(yB-yA, 2) + Math.Pow(zB-zA, 2));
+System.Console.WriteLine($"Длинна отрезка AB = {length}");
